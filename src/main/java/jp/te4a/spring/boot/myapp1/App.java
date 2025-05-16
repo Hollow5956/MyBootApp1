8df3b91dc6837eac278eb57341d6ef92a0fcae9e
石川 陸斗 <220017@jc-21.jp>
2025-05-16 14:28:33 +0900

    t pushコミットメッセージ

--- src/main/java/jp/te4a/spring/boot/myapp1/App.java
+++ src/main/java/jp/te4a/spring/boot/myapp1/App.java
@@ -1,4 +1,5 @@
 package jp.te4a.spring.boot.myapp1;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
@@ -14,8 +15,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class App 
 {
- public static void main( String[] args )
- {
- ApplicationContext ctx = SpringApplication.run(App.class, args);
- }
+    public static void main( String[] args )
+    {
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
+    }
 }
